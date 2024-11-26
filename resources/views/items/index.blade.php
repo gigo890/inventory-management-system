c6a7b705de77cd82e86ed770778fda8b4770ff4c
gigo890 <149149857+gigo890@users.noreply.github.com>
2024-11-26 15:14:51 +0000

    Item index fixes

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -5,16 +5,33 @@
         </h2>
     </x-slot>
 
+    {{-- SEARCH --}}
+    <div class="w-full">
+    <form class="max-w-md mx-auto w-6/12 mt-2 bg-white">
+        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+        <div class="relative">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                </svg>
+            </div>
+            <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+            <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+        </div>
+    </form>
+    </div>
+
+    {{-- CONTENT --}}
    <div class="py-12">
-        <div class="flex flex-row flex-wrap justify-center max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <div class="flex flex-row flex-wrap justify-center content-center max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
 
 
                 @forelse ($items as $item)
-                <div class="flex flex-col flex-wrap m-5 w-[300px] max-w-sm bg-white border border-gray-200 rounded-lg  dark:border-gray-700">
+                <div class="flex flex-col flex-wrap m-5 w-[300px] h-[550px] max-w-sm max-h-md bg-white border border-gray-200 rounded-lg rounded-lg dark:border-gray-700">
                     <a href="#" >
                     <img class="rounded-t-lg object-fit:cover rounded-t-lg " src="{{ ($item->image_path)  }}" width="100%" height="250" alt="" />
                     </a>
-                    <div class="p-5 shadow dark:border-gray-800 rounded-lg w=100% h=100%" >
+                    <div class="p-5  w=100% h=100%" >
                         <a href="#">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-800">{{ $item->name }}</h5>
                         </a>
