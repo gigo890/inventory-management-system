c9fa69c985c982068ce6d34bf1d31d271718869e
gigo890 <149149857+gigo890@users.noreply.github.com>
2024-11-29 15:22:54 +0000

    Inventory tweaks

--- resources/views/items/inventory.blade.php
+++ resources/views/items/inventory.blade.php
@@ -18,17 +18,17 @@
             </tr>
             @forelse($items as $item)
                 <tr>
-                    <td>{{$item->id}}</td>
-                    <td>{{$item->name}}</td>
-                    <td>{{$item->image_path}}</td>
-                    <td>{{$item->description}}</td>
-                    <td>{{$item->price}}</td>
-                    <td>{{$item->stock_amount}}</td>
-                    <td>{{$item->reserved_amount}}</td>
+                    <td>{{ ($item->id)}}</td>
+                    <td>{{($item->name)}}</td>
+                    <td>{{($item->image_path)}}</td>
+                    <td>{{($item->description)}}</td>
+                    <td>{{($item->price)}}</td>
+                    <td>{{($item->stock_amount)}}</td>
+                    <td>{{($item->reserved_amount)}}</td>
                 </tr>
             @empty
                 <p>There are no items to display</p>
-            @endforeach
+            @endforelse
         </table>
     </div>
 </x-app-layout>
