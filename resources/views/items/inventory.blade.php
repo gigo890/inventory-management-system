3dfb998afc5c6041f7df0f59b1f2c7286ff3ef25
gigo890 <149149857+gigo890@users.noreply.github.com>
2024-12-02 14:57:12 +0000

    Added Create Item Button to inventory.blade.php

--- resources/views/items/inventory.blade.php
+++ resources/views/items/inventory.blade.php
@@ -6,6 +6,9 @@
     </x-slot>
 
    <div class="py-12">
+        <button type="button" href="{{ route('items.create') }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            + Add New Item
+        </button>
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
             <table class="w-full text-sm text-left rtl:text-right dark:text-gray-500 dark:text-gray-400">
                 <thead class="text-xs dark:text-gray-700 uppercase dark:text-gray-400 dark:bg-gray-200">
