ebccb91e79b2495c472b17ba64d59b7835830dd0
gigo890 <149149857+gigo890@users.noreply.github.com>
2024-12-06 17:32:09 +0000

    Navigation Colour Tweaks

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100 dark:border-gray-50 dark:bg-gray-800 text-white">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('items.index') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-white" />
                     </a>
                 </div>
 
@@ -15,9 +15,16 @@
                     <x-nav-link :href="route('items.index')" :active="request()->routeIs('items.index')">
                         {{ __('Items') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('inventory')" :active="request()->routeIs('inventory')">
-                        {{ __('Inventory') }}
-                    </x-nav-link>
+                    {{--  CHECKING IF USER IS ADMIN
+                    @if(Auth::user()->)--}}
+                        <x-nav-link :href="route('inventory')" :active="request()->routeIs('inventory')">
+                            {{ __('Inventory') }}
+                        </x-nav-link>
+                    {{--
+                        <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
+                            {{ __('Users') }}
+                        </x-nav-link>
+                    @endif --}}
                 </div>
             </div>
 
