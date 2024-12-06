9e5c3d5c03ff572eeda205c80297f377abdda06c
gigo890 <149149857+gigo890@users.noreply.github.com>
2024-12-06 19:00:26 +0000

    typo

--- resources/views/items/show.blade.php
+++ resources/views/items/show.blade.php
@@ -22,7 +22,7 @@
                     <div class="w-fill m-5 flex justify-center"><a href="#" class="w-6/12 text-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Reserve</a></div>
 
                     <h1 class="mb-2 m-x-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white border-b-2 border-gray-800 dark:border-white ">{{ ($item->name) }}</h1>
-                    <h5 class="mt-2 border-b-2 border-gray-700 dark:border-white font-bold">Dimensions: </h5>
+                    <h5 class="mt-2 border-b-2 border-gray-700 dark:border-white font-bold">Dimensions:</h5>
                     <p>{{ ($item->dimensions) }}</p>
                     <h5 class="mt-2 border-b-2 border-gray-700 dark:border-white font-bold">Description:</h5>
                     <p class="mt-2">{{ ($item->description) }}</p>
