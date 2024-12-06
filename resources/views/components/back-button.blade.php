e58add0fd5706223321afb648b6ab660d005a4bf
gigo890 <149149857+gigo890@users.noreply.github.com>
2024-12-06 17:31:44 +0000

    Added Back Buttons

---
+++ resources/views/components/back-button.blade.php
@@ -0,0 +1,7 @@
+
+<a href="javascript:history.back()" type="button" class=" text-white bg-gray-700 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-gray-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+      </svg>
+    <span class="sr-only">Return</span>
+</a>
