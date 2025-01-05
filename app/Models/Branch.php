85f30795367721832ff90ff9c9430761a4a86151
gigo890 <149149857+gigo890@users.noreply.github.com>
2025-01-05 23:17:37 +0000

    branch seeding fixes

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Branch extends Model
 {
-    //
+    protected $guarded = [];
+
+    use HasFactory;
+
+    public function users(){
+        return $this->hasMany(User::class);
+    }
+    public function items(){
+        return $this->hasMany(Item::class);
+    }
 }
