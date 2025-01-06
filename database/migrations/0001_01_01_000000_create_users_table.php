af0031e39c81b8c0ee15467d355f328f84fbd804
gigo890 <149149857+gigo890@users.noreply.github.com>
2025-01-06 00:52:40 +0000

    user migration fixes

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('role')->default('Staff');
             $table->string('status')->default('Active');
             $table->integer('branch_id');
             $table->timestamps();
